44345101d617a5d040b6192f5b920fe6e7d3ee32
offwork <chomunist@gmail.com>
2020-03-05 11:45:44 +0300

    update config

--- apps/bookshop-e2e/src/integration/app.spec.ts
+++ apps/bookshop-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('bookhop', () => {
+describe('bookshop', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('bookhop', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to bookhop!');
+    getGreeting().contains('Welcome to bookshop!');
   });
 });
